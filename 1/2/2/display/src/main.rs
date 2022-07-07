baaf616ea0d93eaedc16b38deedc0f2ab8b70ff4
Saikat Das <saikatdas0790@gmail.com>
2022-07-07 15:32:41 +0530

    1.2.2. display

--- 1/2/2/display/src/main.rs
+++ 1/2/2/display/src/main.rs
@@ -21,6 +21,18 @@ impl fmt::Display for Point2D {
     }
 }
 
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    imag: f64,
+}
+
+impl fmt::Display for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} + {}i", self.real, self.imag)
+    }
+}
+
 fn main() {
     let minmax = MinMax(0, 14);
 
@@ -38,4 +50,13 @@ fn main() {
     println!("Compare points:");
     println!("Display: {}", point);
     println!("Debug: {:?}", point);
+
+    let imaginary = Complex {
+        real: 3.3,
+        imag: 7.2,
+    };
+
+    println!("Compare complex numbers:");
+    println!("Display: {}", imaginary);
+    println!("Debug: {:?}", imaginary);
 }
