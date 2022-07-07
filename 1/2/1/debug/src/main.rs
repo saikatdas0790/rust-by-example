2e4a149124726b56bfc8c473d0b79f09cbcd8292
Saikat Das <saikatdas0790@gmail.com>
2022-07-07 12:05:24 +0530

    1.2.1 debug

--- 1/2/1/debug/src/main.rs
+++ 1/2/1/debug/src/main.rs
@@ -1,21 +1,16 @@
 use std::fmt::Debug;
 
 #[derive(Debug)]
-struct Structure(i32);
-
-#[derive(Debug)]
-struct Deep(Structure);
+#[allow(dead_code)]
+struct Person<'a> {
+    name: &'a str,
+    age: u8,
+}
 
 fn main() {
-    println!("{:?} months in a year.", 12);
-    println!(
-        "{1:?} {0:?} is the {actor:?} name.",
-        "Slater",
-        "Christian",
-        actor = "actor's"
-    );
+    let name = "Peter";
+    let age = 27;
+    let peter = Person { name, age };
 
-    println!("Now {:?} will print!", Structure(3));
-
-    println!("Now {:?} will print!", Deep(Structure(7)));
+    println!("{:#?}", peter);
 }
