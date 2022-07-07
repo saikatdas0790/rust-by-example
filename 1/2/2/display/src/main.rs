da4c405afd7996b13876d2fca634d99b17ef7e45
Saikat Das <saikatdas0790@gmail.com>
2022-07-07 14:15:41 +0530

    1.2.2 display

---
+++ 1/2/2/display/src/main.rs
@@ -0,0 +1,13 @@
+use std::fmt;
+
+struct Structure(i32);
+
+impl fmt::Display for Structure {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
