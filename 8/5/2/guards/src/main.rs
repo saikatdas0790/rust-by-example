ff136d39e6386f8c11179b02e206ac06128d1d96
Saikat Das <saikatdas0790@gmail.com>
2022-08-07 11:42:33 +0530

    8.5.2. Guards

---
+++ 8/5/2/guards/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let pair = (2, -2);
+
+    println!("Tell me about {:?}", pair);
+    match pair {
+        (x, y) if x == y => println!("These are twins"),
+        (x, y) if x + y == 0 => println!("Antimatter, kaboom!"),
+        (x, _) if x % 2 == 0 => println!("The first one is even"),
+        _ => println!("No correlation..."),
+    }
+
+    let number: u8 = 4;
+
+    match number {
+        i if i == 0 => println!("Zero"),
+        i if i > 0 => println!("Greater than zero"),
+        _ => println!("Fell through"),
+    }
+}
