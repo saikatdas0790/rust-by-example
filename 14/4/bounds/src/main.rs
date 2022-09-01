3a08b7cc458105cc30ea10dcf471788e0e42e28a
Saikat Das <saikatdas0790@gmail.com>
2022-09-01 17:05:32 +0530

    14.4. Bounds

---
+++ 14/4/bounds/src/main.rs
@@ -0,0 +1,51 @@
+use std::fmt::{Debug, Display};
+
+fn printer<T: Display>(t: T) {
+    println!("{}", t);
+}
+
+struct S<T: Display>(T);
+
+trait HasArea {
+    fn area(&self) -> f64;
+}
+
+impl HasArea for Rectangle {
+    fn area(&self) -> f64 {
+        self.length * self.height
+    }
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    length: f64,
+    height: f64,
+}
+#[allow(dead_code)]
+struct Triangle {
+    length: f64,
+    height: f64,
+}
+
+fn print_debug<T: Debug>(t: &T) {
+    println!("{:?}", t);
+}
+
+fn area<T: HasArea>(t: &T) -> f64 {
+    t.area()
+}
+
+fn main() {
+    // let s = S(vec![1]);
+    let rectangle = Rectangle {
+        length: 3.0,
+        height: 4.0,
+    };
+    let _triangle = Triangle {
+        length: 3.0,
+        height: 4.0,
+    };
+
+    print_debug(&rectangle);
+    println!("Area: {}", rectangle.area());
+}
