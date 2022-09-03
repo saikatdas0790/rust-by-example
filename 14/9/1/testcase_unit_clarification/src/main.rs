197ff4a2b5f26d3f72742097e45cdd8295d75e8c
Saikat Das <saikatdas0790@gmail.com>
2022-09-03 22:03:12 +0530

    14.9.1. Testcase: unit clarification

---
+++ 14/9/1/testcase_unit_clarification/src/main.rs
@@ -0,0 +1,32 @@
+use std::{marker::PhantomData, ops::Add};
+
+#[derive(Debug, Clone, Copy)]
+enum Inch {}
+
+#[derive(Debug, Clone, Copy)]
+enum Mm {}
+
+#[derive(Debug, Clone, Copy)]
+struct Length<Unit>(f64, PhantomData<Unit>);
+
+impl<Unit> Add for Length<Unit> {
+    type Output = Length<Unit>;
+
+    fn add(self, rhs: Length<Unit>) -> Self::Output {
+        Length(self.0 + rhs.0, PhantomData)
+    }
+}
+
+fn main() {
+    let one_foot: Length<Inch> = Length(12.0, PhantomData);
+    let one_meter: Length<Mm> = Length(1000.0, PhantomData);
+
+    let two_feet = one_foot + one_foot;
+    let two_meters = one_meter + one_meter;
+
+    println!("one foot + one_foot = {:?}", two_feet);
+    println!("one meter + one_meter = {:?}", two_meters);
+
+    // Compile-time Error: type mismatch
+    // let one_feter = one_foot + one_meter;
+}
