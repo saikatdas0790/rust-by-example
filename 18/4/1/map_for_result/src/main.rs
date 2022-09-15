db341947c4e9b88167456bd0d6c0a6279e00ec70
Saikat Das <saikatdas0790@gmail.com>
2022-09-15 09:37:19 +0530

    18.4.1. map for Result

--- 18/4/1/map_for_result/src/main.rs
+++ 18/4/1/map_for_result/src/main.rs
@@ -1,13 +1,11 @@
 use std::num::ParseIntError;
 
 fn multiply(first_number_str: &str, second_number_str: &str) -> Result<i32, ParseIntError> {
-    match first_number_str.parse::<i32>() {
-        Ok(first_number) => match second_number_str.parse::<i32>() {
-            Ok(second_number) => Ok(first_number * second_number),
-            Err(e) => Err(e),
-        },
-        Err(e) => Err(e),
-    }
+    first_number_str.parse::<i32>().and_then(|first_number| {
+        second_number_str
+            .parse::<i32>()
+            .map(|second_number| first_number * second_number)
+    })
 }
 
 fn print(result: Result<i32, ParseIntError>) {
