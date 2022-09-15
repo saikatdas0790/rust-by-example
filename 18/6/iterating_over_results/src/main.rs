f817426af26876250e098830c75a59df30df4d80
Saikat Das <saikatdas0790@gmail.com>
2022-09-15 16:47:55 +0530

    Fail the entire operation with collect()

--- 18/6/iterating_over_results/src/main.rs
+++ 18/6/iterating_over_results/src/main.rs
@@ -1,11 +1,5 @@
 fn main() {
-    let strings = vec!["42", "tofu", "93", "999", "18"];
-    let mut errors = vec![];
-    let numbers: Vec<_> = strings
-        .into_iter()
-        .map(|s| s.parse::<u8>())
-        .filter_map(|r| r.map_err(|e| errors.push(e)).ok())
-        .collect();
+    let strings = vec!["tofu", "93", "18"];
+    let numbers: Result<Vec<_>, _> = strings.into_iter().map(|s| s.parse::<i32>()).collect();
     println!("Results: {:?}", numbers);
-    println!("Errors: {:?}", errors);
 }
