565f32babf95a0eace59a45293e6021baf357545
Saikat Das <saikatdas0790@gmail.com>
2022-09-15 15:06:30 +0530

    18.5.1. Pulling Results out of Options

--- 18/5/1/pulling_results_out_of_options/src/main.rs
+++ 18/5/1/pulling_results_out_of_options/src/main.rs
@@ -1,7 +1,9 @@
 use std::num::ParseIntError;
 
-fn double_first(vec: Vec<&str>) -> Option<Result<i32, ParseIntError>> {
-    vec.first().map(|first| first.parse::<i32>().map(|n| 2 * n))
+fn double_first(vec: Vec<&str>) -> Result<Option<i32>, ParseIntError> {
+    let opt = vec.first().map(|first| first.parse::<i32>().map(|n| 2 * n));
+
+    opt.map_or(Ok(None), |r| r.map(Some))
 }
 
 fn main() {
