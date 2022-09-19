1301de41053dd1dac30c45cbb361637922b4108b
Saikat Das <saikatdas0790@gmail.com>
2022-09-19 08:20:03 +0530

    19.3. Strings

--- 19/3/strings/src/main.rs
+++ 19/3/strings/src/main.rs
@@ -1,3 +1,5 @@
+use std::str;
+
 fn main() {
     let pangram = "the quick brown fox jumps over the lazy dog";
     println!("Pangram: {}", pangram);
@@ -26,4 +28,53 @@ fn main() {
 
     println!("Alice says: {}", alice);
     println!("Bob says: {}", bob);
+
+    let byte_escape = "I'm writing \x52\x75\x73\x74!";
+    println!("What are you doing\x3F (\\x3F means ?) {}", byte_escape);
+
+    let unicode_codepoint = "\u{211D}";
+    let character_name = "\"DOUBLE-STRUCK CAPITAL R\"";
+
+    println!(
+        "Unicode character {} (U+211D) is called {}",
+        unicode_codepoint, character_name
+    );
+
+    let long_string = "String literals
+can span multiple lines.
+The linebreak and indentation here ->\
+<- can be escaped too!";
+    println!("{}", long_string);
+
+    let raw_str = r"Escapes don't work here: \x3F \u{211D}";
+    println!("{}", raw_str);
+
+    let quotes = r#"And then I said: "There is no escape!""#;
+    println!("{}", quotes);
+
+    let longer_delimiter = r###"A string with "# in it. And even "##!"###;
+    println!("{}", longer_delimiter);
+
+    let bytestring = b"this is a byte string";
+    println!("A byte string: {:?}", bytestring);
+
+    let escaped = b"\x52\x75\x73\x74 as bytes";
+    println!("Some escaped bytes: {:?}", escaped);
+
+    let raw_bytestring = br"\u{211D} is not escaped here";
+    println!("Some escaped bytes: {:?}", raw_bytestring);
+
+    if let Ok(my_str) = str::from_utf8(raw_bytestring) {
+        println!("And the same as text: '{}'", my_str);
+    }
+
+    let _quotes = br#"You can also use "fancier" formatting, \
+                     like with normal raw strings"#;
+
+    let shift_jis = b"\x82\xe6\x82\xa8\x82\xb1\x82";
+
+    match str::from_utf8(shift_jis) {
+        Ok(my_str) => println!("Conversion successful: '{}'", my_str),
+        Err(e) => println!("Conversion failed: {:?}", e),
+    }
 }
