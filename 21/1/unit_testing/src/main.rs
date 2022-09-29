c1f6145adba787560d4ddc02122cb0adbc26e1fd
Saikat Das <saikatdas0790@gmail.com>
2022-09-29 16:22:35 +0530

    Testing panics

--- 21/1/unit_testing/src/main.rs
+++ 21/1/unit_testing/src/main.rs
@@ -1,9 +1,10 @@
-fn sqrt(number: f64) -> Result<f64, String> {
-    if number >= 0.0 {
-        Ok(number.powf(0.5))
-    } else {
-        Err("negative floats don't have square roots.".to_owned())
+pub fn divide_non_zero_result(a: u32, b: u32) -> u32 {
+    if b == 0 {
+        panic!("Divide-by-zero error");
+    } else if a < b {
+        panic!("Divide result is zero");
     }
+    a / b
 }
 
 #[cfg(test)]
@@ -11,10 +12,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_sqrt() -> Result<(), String> {
-        let x = 4.0;
-        assert_eq!(sqrt(x)?.powf(2.0), x);
-        Ok(())
+    fn test_divide() {
+        assert_eq!(divide_non_zero_result(10, 2), 5);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_any_panic() {
+        divide_non_zero_result(1, 0);
+    }
+
+    #[test]
+    #[should_panic(expected = "Divide result is zero")]
+    fn test_divide_non_zero_result() {
+        divide_non_zero_result(1, 10);
     }
 }
 
