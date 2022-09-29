b3724a4eb19d4f2bd4b1eff2c6b148405045c290
Saikat Das <saikatdas0790@gmail.com>
2022-09-29 16:19:25 +0530

    Tests and ?

--- 21/1/unit_testing/src/main.rs
+++ 21/1/unit_testing/src/main.rs
@@ -1,10 +1,9 @@
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
-
-#[allow(dead_code)]
-fn bad_add(a: i32, b: i32) -> i32 {
-    a - b
+fn sqrt(number: f64) -> Result<f64, String> {
+    if number >= 0.0 {
+        Ok(number.powf(0.5))
+    } else {
+        Err("negative floats don't have square roots.".to_owned())
+    }
 }
 
 #[cfg(test)]
@@ -12,13 +11,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_add() {
-        assert_eq!(add(1, 2), 3);
-    }
-
-    #[test]
-    fn test_bad_add() {
-        assert_eq!(bad_add(1, 2), 3);
+    fn test_sqrt() -> Result<(), String> {
+        let x = 4.0;
+        assert_eq!(sqrt(x)?.powf(2.0), x);
+        Ok(())
     }
 }
 
